9c7c0935e00f89321f737db3e31d0808cbf2dbbd
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-05 22:16:50 +0200

    feat: Add solution to check for duplicate numbers in a list

---
+++ 217. Contains Duplicate/hashMap.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        anagrams = defaultdict(int)
+        for i in nums:
+            anagrams[i] += 1 
+        
+        for value in anagrams.values():
+            if value > 1:
+                return True
+        return False
+        
+def main():
+    sol = Solution()
+    nums = [1,2,3,1]
+    print(sol.containsDuplicate(nums))
+
+if __name__ == "__main__":
+    main()    
