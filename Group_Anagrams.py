740c53cef557a6b450d09c1e45bf9f81078ff8f2
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-05 21:31:51 +0200

    add class:Solution

--- Group_Anagrams.py
+++ Group_Anagrams.py
@@ -1,5 +1,24 @@
-main():
+from collections import defaultdict
+from typing import List
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        result = []
+
+        for s in strs:
+            sorted_str = tuple(sorted(s))
+            anagrams[sorted_str].append(s)
+            
+        for value in anagrams.values():
+            result.append(value)
+        
+        return result
+
+def main():
+  sol = Solution()
+  strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+  print(sol.groupAnagrams(strs))
 
 if __name__ == "__main__":
   main()
