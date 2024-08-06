b71a048478576f4bd699e09a78ae64ade2a520af
notSaqlain <117275944+notSaqlain@users.noreply.github.com>
2024-08-06 21:40:45 +0200

    Create HashMap.py

---
+++ 1. Two Sum/HashMap.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        Hmap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in Hmap:
+                return [Hmap[diff], i]
+            Hmap[n] = i
+        return
+
+def main():
+    sum = Solution()
+    print(sum.twoSum([2, 7, 11, 15], 9)) # [0, 1]
+
+
+if __name__ == "__main__":
+    main()
