f0eb5a310180cd510b6bb485c8e6dd3ca6d978fa
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-06 21:56:55 +0200

    feat: Implement twoSum method in Solution class

---
+++ 167. Two Sum II - Input Array Is Sorted/main.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers) - 1
+        while l < r:
+            if numbers[l] + numbers[r] > target:
+                r -=1
+            elif numbers[l] + numbers[r] < target:
+                l+=1
+            else:
+                return [l+1, r+1]
+        return[l+1, r+1]
+
+def main():
+    sum = Solution()
+    print(sum.twoSum([2, 7, 11, 15], 9)) # [1, 2]
+
+if __name__ == "__main__":
+    main()
