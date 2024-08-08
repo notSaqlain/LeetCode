279065054b229ff6c55179e07344afdd73fbd549
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-08 20:51:14 +0200

    feat: Add findMedianSortedArrays method to Solution class

---
+++ 4. Median of Two Sorted Arrays/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        lists = nums1 + nums2
+        somma, l = 0, len(lists)
+        for n in lists:
+            somma += n
+        
+        return somma/l
+
+def main():
+    map_sol= Solution()
+    print(map_sol.findMedianSortedArrays([1,3], [2]))
+    print(map_sol.findMedianSortedArrays([1,2], [3,4]))
+    print(map_sol.findMedianSortedArrays([0,0], [0,0]))
+    print(map_sol.findMedianSortedArrays([], [1]))
+    print(map_sol.findMedianSortedArrays([2], []))
+
+if __name__ == "__main__":
+    main()
