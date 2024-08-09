24ec1624f948db3be713ee1a03b180ee8420e894
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-09 21:17:41 +0200

    refactor findMedianSortedArrays method in Solution class

---
+++ 4. Median of Two Sorted Arrays/TimeComplexity.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        merged = nums1 + nums2
+        merged.sort()
+        total = len(merged)
+
+        if total % 2 == 1:
+            return float(merged[total // 2])
+        else:
+            middle1 = merged[total // 2 - 1]
+            middle2 = merged[total // 2]
+            return (float(middle1) + float(middle2)) / 2.0
+        
+def main():
+    map_sol= Solution()
+    print(map_sol.findMedianSortedArrays([1,3], [2]))
+    print(map_sol.findMedianSortedArrays([1,2], [3,4]))
+    print(map_sol.findMedianSortedArrays([0,0], [0,0]))
+    print(map_sol.findMedianSortedArrays([], [1]))
+    print(map_sol.findMedianSortedArrays([2], []))
+
+if __name__ == "__main__":
+    main()
