b7e054c6ef4623c3bbe15c4f9a1bc82a9d7e68f7
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-13 22:53:05 +0200

    feat: Add plusOne method to Solution class

---
+++ 66. Plus One/main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = int("".join(map(str, digits)))
+        number += 1
+        array = [int(digit) for digit in str(number)]
+        return array
+
+
+def main():
+    s = Solution()
+    print(s.plusOne([1,2,3])) # [1,2,4]
+    print(s.plusOne([4,3,2,1])) # [4,3,2,2]
+    print(s.plusOne([0])) # [1]
+
+if __name__ == "__main__":
+    main()
