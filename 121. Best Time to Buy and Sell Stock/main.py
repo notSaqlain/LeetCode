e4353bfcaf37d2719d2b0b90cf370e9191d2e623
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-17 22:38:44 +0200

    feat: Optimize maxProfit function for better performance

---
+++ 121. Best Time to Buy and Sell Stock/main.py
@@ -0,0 +1,22 @@
+# ERROR: Time Limit Exceeded
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_val = 0
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[i] < prices[j]:
+                    if prices[j] - prices[i] > max_val:
+                        max_val = prices[j] - prices[i]
+
+        return max_val
+    
+def main():
+    sol = Solution()
+    prices = [7,1,5,3,6,4]
+    print(sol.maxProfit(prices)) # 5
+
+if __name__ == "__main__":
+    main()
