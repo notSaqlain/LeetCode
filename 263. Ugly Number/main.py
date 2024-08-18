f6af3d5868fcf2525a5a88794dc54ba90d4301e6
notSaqlain <iamnotsaqlain@gmail.com>
2024-08-18 21:27:08 +0200

    feat: Optimize isUgly function for better performance

---
+++ 263. Ugly Number/main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n < 1:
+            return False
+        for i in range(2, 6):
+            while n%i == 0:
+                n /= i
+
+        return n==1
+
+# Time complexity: O(logn)
+# Space complexity: O(1)
+
+def main():
+    n = 14
+    s = Solution()
+    print(s.isUgly(n))
+
+if __name__ == '__main__':
+    main()
